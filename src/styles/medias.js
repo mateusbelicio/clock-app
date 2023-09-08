71eed3a9f03d66515649b3cd7ffbd62c64728924
Mateus Belicio <mateusbelicio@hotmail.com>
2023-09-08 09:45:15 -0300

    ✨: setting breakpoints to use in media queries

---
+++ src/styles/medias.js
@@ -0,0 +1,12 @@
+export function breakpoint(size, maxWidth = false) {
+  const deviceSizes = {
+    sm: 640,
+    md: 768,
+    lg: 940,
+    xl: 1158,
+  };
+
+  return maxWidth
+    ? `(max-width: ${deviceSizes[size] / 16}rem)`
+    : `(min-width: ${deviceSizes[size] / 16}rem)`;
+}
