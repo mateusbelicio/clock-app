7b5c98840dcfbd906b24714938ff67204d069edf
Mateus Belicio <mateusbelicio@hotmail.com>
2023-09-07 22:56:10 -0300

    ✨: icon component

---
+++ src/ui/Icon.jsx
@@ -0,0 +1,46 @@
+import styled, { css } from 'styled-components';
+
+import icons from '@/assets/icons.svg';
+
+const colors = {
+  white: css`
+    color: var(--clr-neutral-50);
+  `,
+  gray: css`
+    color: var(--clr-neutral-500);
+  `,
+  black: css`
+    color: var(--clr-neutral-950);
+  `,
+};
+
+const StyledIcon = styled.span`
+  font-size: ${({ size }) => size / 16}rem;
+  ${({ color }) => color && colors[color]};
+  ${({ hover }) =>
+    hover &&
+    css`
+      &:hover {
+        ${colors[hover]};
+      }
+    `}
+
+  transition: color 250ms;
+
+  svg {
+    width: 1em;
+    height: 1em;
+  }
+`;
+
+function Icon({ name, size = 16, hover, color }) {
+  return (
+    <StyledIcon {...{ size, hover, color }}>
+      <svg>
+        <use xlinkHref={`${icons}#icon-${name}`} />
+      </svg>
+    </StyledIcon>
+  );
+}
+
+export default Icon;
