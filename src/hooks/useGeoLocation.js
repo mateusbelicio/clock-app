3425829db975d3e124c1ed9d730fc6f48c436c04
Mateus Belicio <mateusbelicio@hotmail.com>
2023-09-11 10:38:08 -0300

    ✨: geolocation based on the client's IP address

---
+++ src/hooks/useGeoLocation.js
@@ -0,0 +1,30 @@
+import { useEffect, useState } from 'react';
+import Ipbase from '@everapi/ipbase-js';
+
+const ipBase = new Ipbase(import.meta.env.VITE_IPBASE_KEY);
+
+export function useGeoLocation() {
+  const [location, setLocation] = useState({ city: '', country: '' });
+
+  useEffect(() => {
+    const getIpInfo = async () => {
+      try {
+        const response = await ipBase.info();
+        if (!response) throw new Error('Error fetching data.');
+        console.log(response);
+
+        const city = response.data.location.city.name;
+        const country = response.data.location.country.alpha2;
+
+        setLocation({ city, country });
+      } catch (error) {
+        console.error('Something went wrong: ', error);
+        return null;
+      }
+    };
+
+    getIpInfo();
+  }, []);
+
+  return location;
+}
