1ec2ef2677cb9a13632bea09e6ecf884dc9e204e
Mateus Belicio <mateusbelicio@hotmail.com>
2023-09-11 11:19:48 -0300

    ✨: information opening/closing functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,20 @@
+import { useState } from 'react';
+
+import AppLayout from './ui/AppLayout';
+import Quotes from './features/quotes/Quotes';
+import Clock from './features/clock/Clock';
+
 function App() {
+  const [isOpenInfo, setIsOpenInfo] = useState(false);
+
   return (
-    <div>
-      <div className="message">
-        <div>
-          <p>
-            “The science of operations, as derived from mathematics more
-            especially, is a science of itself, and has its own abstract truth
-            and value.”
-          </p>
-          <h5>Ada Lovelace</h5>
-        </div>
-        <button>Refresh</button>
-      </div>
-
-      <main>
-        <section className="clock">
-          <h1>
-            <span>icon</span> GOOD MORNING, IT’S CURRENTLY{' '}
-            <span>11:37 bst</span> <span>IN LONDON, UK</span>
-          </h1>
-          <button>MORE</button>
-        </section>
-
-        <section className="info">
-          <ul>
-            <li>
-              <h2>Current timezone</h2>
-              <p>Europe/London</p>
-            </li>
-            <li>
-              <h2>Day of the year</h2>
-              <p>295</p>
-            </li>
-            <li>
-              <h2>Day of the week</h2>
-              <p>5</p>
-            </li>
-            <li>
-              <h2>Week number</h2>
-              <p>42</p>
-            </li>
-          </ul>
-        </section>
-      </main>
-    </div>
+    <AppLayout>
+      {!isOpenInfo && <Quotes />}
+      <Clock
+        isOpenInfo={isOpenInfo}
+        toggleInfo={() => setIsOpenInfo((isOpen) => !isOpen)}
+      />
+    </AppLayout>
   );
 }
 
