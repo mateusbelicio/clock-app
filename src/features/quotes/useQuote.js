60b5f7dbc963ffb48a65f537abb16ab0c38df4ac
Mateus Belicio <mateusbelicio@hotmail.com>
2023-09-14 18:04:30 -0300

    🐛: setting initial state to empty string

--- src/features/quotes/useQuote.js
+++ src/features/quotes/useQuote.js
@@ -4,8 +4,8 @@ import { useEffect, useState } from 'react';
 const QUOTE_API_URL = 'https://api.quotable.io/quotes/random';
 
 const initialState = {
-  text: '“The science of operations, as derived from mathematics more especially, is a science of itself, and has its own abstract truth and value.”',
-  author: 'Adam Lovelace',
+  text: '',
+  author: '',
 };
 
 export function useQuote() {
@@ -13,10 +13,10 @@ export function useQuote() {
   const { data, loading, error, update } = useFetch(QUOTE_API_URL);
 
   useEffect(() => {
-    if (!data) return;
+    if (!data || loading) return;
 
     setQuote({ text: data[0].content, author: data[0].author });
-  }, [data]);
+  }, [data, loading]);
 
   return { quote, isLoading: loading, error, updateQuote: update };
 }
